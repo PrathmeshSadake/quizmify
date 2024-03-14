5ad19caedcf5354b249f41da5392c684d97bd832
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-03-14 10:05:39 +0530

    Update AI model and API key in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { generateWithOpenAI } from "./models/openai-m";
+import { generateWithTogetherAI } from "./models/togetherai-m";
 
 import "dotenv/config";
 
@@ -7,9 +7,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", async (req: Request, res: Response) => {
-  const data = await generateWithOpenAI({
+  const data = await generateWithTogetherAI({
     topic: "General Knowledge",
-    apiKey: process.env.OPENAI_API_KEY as string,
+    apiKey: process.env.TOGETHER_API_KEY as string,
   });
 
   return res.status(200).send(data);
---
+++ src/models/togetherai-m.ts
@@ -0,0 +1,50 @@
+import OpenAI from "openai";
+
+export async function generateWithTogetherAI({
+  topic,
+  amount = 5,
+  apiKey,
+  options_length,
+}: {
+  topic: string;
+  amount?: number;
+  apiKey: string;
+  options_length?: number;
+}) {
+  const client = new OpenAI({
+    apiKey,
+    baseURL: "https://api.together.xyz/v1",
+  });
+  const mcqStructure = [
+    {
+      question: "What is the capital of France?",
+      options: [
+        {
+          title: "New York",
+          isCorrect: "false",
+        },
+      ],
+      correct_answer: "Paris",
+    },
+  ];
+  const response = await client.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant designed to output JSON.",
+      },
+      {
+        role: "user",
+        content: `Generate the ${amount} MCQ questions for the given topic: ${topic}. Keep Options limit is ${options_length} and Follow this json format: ${JSON.stringify(
+          mcqStructure
+        )}`,
+      },
+    ],
+    model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    response_format: { type: "json_object" },
+    n: 1,
+  });
+
+  const output = response.choices[0].message.content;
+  return output;
+}
