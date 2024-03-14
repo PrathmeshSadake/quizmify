d7a4d837985ca416582e4763bf949e42551cb015
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-03-14 14:39:22 +0530

    Refactor import statements in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
-import { generateWithTogetherAI } from "./models/togetherai-m";
 import { generateWithOpenAI } from "./models/openai-m";
 import { generateWithGemini } from "./models/gemini-m";
+import { generateWithTogetherAI } from "./models/togetherai-m";
 
 import "dotenv/config";
 
@@ -14,7 +14,6 @@ app.get("/", async (req: Request, res: Response) => {
     apiKey: process.env.OPENAI_API_KEY as string,
   });
 
-  console.log(data);
   return res.status(200).send(data);
 });
 
@@ -24,7 +23,6 @@ app.get("/together", async (req: Request, res: Response) => {
     apiKey: process.env.TOGETHER_API_KEY as string,
   });
 
-  console.log(data);
   return res.status(200).send(data);
 });
 
@@ -34,7 +32,6 @@ app.get("/gemini", async (req: Request, res: Response) => {
     apiKey: process.env.GEMINI_API_KEY as string,
   });
 
-  console.log(data);
   return res.status(200).send(data);
 });
 
